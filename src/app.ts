ccbbfacba4a2c47070f80e72b0ef9d4f0919e958
Gustavo Matheus Morinaga Cardoso <47375774+gmatthewsfeuer@users.noreply.github.com>
2021-03-12 23:21:40 -0300

    Adicionando CORS

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,33 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-const app = express();
+// import routes from './routes';
 
-app.use(express.json());
+class App {
+	public express: express.Application;
 
-export default app;
+	public constructor() {
+		this.express = express();
+		this.middleware();
+		// this.database();
+		// this.routes();
+	}
+
+	private middleware(): void {
+		this.express.use(express.json());
+		this.express.use(cors());
+	}
+
+	private database(): void {
+		mongoose.connect('', {
+			useNewUrlParser: true,
+		});
+	}
+
+	// private routes(): void {
+	// 	this.express.use(routes);
+	// }
+}
+
+export default new App().express;
